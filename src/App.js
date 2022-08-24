4a32547c9d542a61cdc53df26999d87fea2c30e8
Alex Nordin <apnordin@gmail.com>
2022-08-24 17:50:44 -0400

    Initial styling, heatmap feature, clickmap feature, and heatmap toggle button

--- src/App.js
+++ src/App.js
@@ -1,23 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import "./App.css";
 
 function App() {
+  const [allDotsArray, setAllDotsArray] = React.useState([]);
+  const [allClicksArray, setAllClicksArray] = React.useState([]);
+  const [loadDate, setLoadDate] = React.useState(0);
+  const [clickTimesArray, setClickTimesArray] = React.useState([]);
+  const [isHeatmapVisible, setIsHeatmapVisible] = React.useState(false);
+
+  const createCircle = (event) => {
+    const cursorX = event.clientX;
+    const cursorY = event.clientY;
+    const position = [cursorX, cursorY];
+    const allClicksArrayCopy = [...allClicksArray];
+    allClicksArrayCopy.push(position);
+    setAllClicksArray(allClicksArrayCopy);
+  };
+
+  const clickTimer = () => {
+    const clickTimesArrayCopy = [...clickTimesArray];
+    clickTimesArrayCopy.push(Date.now() - loadDate);
+    setClickTimesArray(clickTimesArrayCopy);
+  };
+
+  const createDot = (event) => {
+    const cursorX = event.clientX;
+    const cursorY = event.clientY;
+    const position = [cursorX, cursorY];
+    const allDotsArrayCopy = [...allDotsArray];
+    allDotsArrayCopy.push(position);
+    setAllDotsArray(allDotsArrayCopy);
+  };
+
+  const handleClick = (event) => {
+    createCircle(event);
+    clickTimer();
+  };
+
+  const toggleHeatmap = () => {
+    setIsHeatmapVisible(!isHeatmapVisible);
+  };
+
+  React.useEffect(() => {
+    setLoadDate(Date.now());
+  }, []);
+
+  React.useEffect(() => {
+    console.log(clickTimesArray);
+  }, [clickTimesArray]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" onMouseMove={createDot} onClick={handleClick}>
+      <div class="main-content">
+        <button class="toggle-heatmap" onClick={toggleHeatmap}>
+          View heatmap
+        </button>
+        {isHeatmapVisible && (
+          <div className="heatmap-layer">
+            {allDotsArray.map((dot) => (
+              <div
+                className="dot"
+                style={{
+                  left: `${(dot[0] - 1).toString()}px`,
+                  top: `${(dot[1] - 1).toString()}px`,
+                }}
+              ></div>
+            ))}
+            {allClicksArray.map((click) => (
+              <div
+                className="click-circle"
+                style={{
+                  left: `${(click[0] - 2).toString()}px`,
+                  top: `${(click[1] - 2).toString()}px`,
+                }}
+              ></div>
+            ))}
+          </div>
+        )}
+      </div>
     </div>
   );
 }

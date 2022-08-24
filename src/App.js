4fc8fd41e26a539f74c3168bcbbf2212f7b9f92a
Alex Nordin <apnordin@gmail.com>
2022-08-24 18:32:43 -0400

    Added viewport reports, console click reports, and console right-click reports

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ function App() {
   const [loadDate, setLoadDate] = React.useState(0);
   const [clickTimesArray, setClickTimesArray] = React.useState([]);
   const [isHeatmapVisible, setIsHeatmapVisible] = React.useState(false);
+  const [viewportWidth, setViewportWidth] = React.useState();
+  const [viewportHeight, setViewportHeight] = React.useState();
 
   const createCircle = (event) => {
     const cursorX = event.clientX;
@@ -35,30 +37,49 @@ function App() {
   const handleClick = (event) => {
     createCircle(event);
     clickTimer();
+    const currentDate = new Date();
+    console.log(
+      `On ${currentDate.toLocaleString()}, you clicked on pixel ${
+        event.clientX
+      }x${event.clientY}. This was exactly ${
+        Date.now() - loadDate
+      }ms after loading the page.`
+    );
   };
 
   const toggleHeatmap = () => {
     setIsHeatmapVisible(!isHeatmapVisible);
   };
 
+  const handleContextMenu = (event) => {
+    console.log(
+      `You opened the console, didn't you? You clicked on pixel ${event.clientX}x${event.clientY} and opened that dirty console.`
+    );
+  };
+
   React.useEffect(() => {
     setLoadDate(Date.now());
+    setViewportWidth(window.innerWidth);
+    setViewportHeight(window.innerHeight);
   }, []);
 
-  React.useEffect(() => {
-    console.log(clickTimesArray);
-  }, [clickTimesArray]);
+  // React.useEffect(() => {
+  //   console.log(clickTimesArray);
+  // }, [clickTimesArray]);
 
   return (
-    <div className="App" onMouseMove={createDot} onMouseDown={handleClick}>
+    <div
+      className="App"
+      onMouseMove={createDot}
+      onMouseDown={handleClick}
+      onContextMenu={handleContextMenu}
+    >
       <div className="main-content">
-        <button className="toggle-heatmap" onClick={toggleHeatmap}>
-          View heatmap
-        </button>
         {isHeatmapVisible && (
           <div className="heatmap-layer">
-            {allDotsArray.map((dot) => (
+            {allDotsArray.map((dot, i) => (
               <div
+                key={i}
                 className="dot"
                 style={{
                   left: `${(dot[0] - 1).toString()}px`,
@@ -66,8 +87,9 @@ function App() {
                 }}
               ></div>
             ))}
-            {allClicksArray.map((click) => (
+            {allClicksArray.map((click, i) => (
               <div
+                key={i}
                 className="click-circle"
                 style={{
                   left: `${(click[0] - 2).toString()}px`,
@@ -77,6 +99,15 @@ function App() {
             ))}
           </div>
         )}
+        <div className="viewport-report">
+          <button className="toggle-heatmap" onClick={toggleHeatmap}>
+            View heatmap
+          </button>
+          <p style={{margin: "0px"}}>
+            Your viewport &#40;on load&#41; is {viewportWidth}px x{" "}
+            {viewportHeight}px
+          </p>
+        </div>
       </div>
     </div>
   );
